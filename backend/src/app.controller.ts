73a2daa4bccdf2376074c0a330bd597dc005b915
Kelvin Silva <kelvinsilva.ksv@gmail.com>
2023-09-01 17:38:56 -0300

    wip

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -5,7 +5,7 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('/send-message')
   getHello() {
     return this.appService.getHello();
   }
